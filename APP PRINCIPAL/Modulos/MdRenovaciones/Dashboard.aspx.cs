2526ef935de72fc55541ba78afcb8e9cf75818ee
jorgelg13 <jorgelg132012@gmail.com>
2018-12-10 09:49:04 -0600

    agregando formato de envio de renovacion de poliza

--- APP PRINCIPAL/Modulos/MdRenovaciones/Dashboard.aspx.cs
+++ APP PRINCIPAL/Modulos/MdRenovaciones/Dashboard.aspx.cs
@@ -43,7 +43,21 @@ public partial class Modulos_MdRenovaciones_Dashboard : System.Web.UI.Page
         int id = Convert.ToInt32(GridElRoble.SelectedRow.Cells[1].Text);
         var registro = DBRenovaciones.renovaciones_polizas.Find(id);
         cuerpo = "Saludos Estimad@ asegurado </br>" +
-            "El "+ registro.vigf+"";
+            "<div style=\"text-align: justify\">" +
+            "<p>El (VigFin) vence la anualidad de su póliza (No, de póliza del ACS), la cual le brinda coberturas al TIPO_VEHICULO  MARCA  ANOVEHÍ PLACA (datos del reporte del Roble).<p>" +
+            "<p>Con el afán de realizar el proceso de renovación de una forma más conveniente para todas las partes, a partir de este año, Seguros El Roble realiza la renovación anticipada de su póliza, la cual encontrará adjunta y cuenta con las siguientes condiciones:</p>" +
+            "<p><b>Valor Garantizado</b> de Q.(SUMA_ASEG_RENOV  - del cuadro del Roble); el valor garantizado es fijo y le da la tranquilidad de contar con la suma asegurada adecuada. Por favor revise el Endoso de Valor Garantizado, donde se describen las condiciones que aplican a este beneficio.<p>" +
+            "<p><b>Deducibles:</b> Los deducibles que aplican en la renovación, son: DEDUC_MIN_DANOS para Daños Propios y DEDUC_MIN_ROBO para Robo Total</p>" +
+            "<p><b>Prima a pagar anual:</b> Q. PRIMATOTANUAL (del cuadro del Roble), fraccionada en NO_PAGOS pagos.</p>" +
+            "<p>Las condiciones especiales con que cuenta su póliza, puede revisarlas en el endoso de “Beneficios adicionales Incluidos sin cobro de prima” y consultarnos por cualquier duda sobre las mismas.</p>" +
+            "<p>Debe revisar y verificar que su renovación contenga las coberturas y condiciones contratadas e informarnos inmediatamente de cualquier modificación que debamos efectuar <b>(Artículo 673 del Código de Comercio de Guatemala)</b>.</p>" +
+            "<p>Le recordamos que debe mantener al día sus pagos, para evitar situaciones de no cobertura en caso de ocurrir un siniestro (Artículo 892 del Código de Comercio de Guatemala).</p>" +
+            "<p>Recuerde que, de ocurrir algún siniestro, debe notificarlo inmediatamente a nuestra cabina de emergencias a los teléfonos 2386-3737 o 2326-3737; si lo reporta directo a la Aseguradora, favor indicárnoslo al día hábil siguiente para dar seguimiento a su reclamo.</p>" +
+            "<p>NOTA IMPORTANTE: En caso no recibamos confirmación escrita de que no desea esta renovación, como mínimo con 5 días de anticipación al vencimiento (VigFin), la Aseguradora asumirá que está de acuerdo con los términos y condiciones y procederá a realizar los cobros en las fechas pactadas en el Anexo de Pagos.<p>" +
+            "<p>Agradecemos la confianza depositada en nuestros servicios para el manejo de sus seguros y estoy a las órdenes para cualquier aclaración o consulta.</p>" +
+            "<p>Nombre y Credencial del Ejecutivo de Cuenta</p>" +
+            "<p>Dirección de correo electrónico</p>" +
+            "</div>";
 
         if (!emailValidator.Validate(correo.Trim(), out resultado))
         {
@@ -54,7 +68,7 @@ public partial class Modulos_MdRenovaciones_Dashboard : System.Web.UI.Page
         {
             case EmailValidationResult.OK:
                // Console.WriteLine("Mailbox exists");
-                Correos.Notificacion(correo.Trim(), "Renovacion de poliza", "Su poliza a sido renovada");
+                Correos.Notificacion(correo.Trim(), "Renovacion de poliza", cuerpo);
                 registro.estado = 3;
                 DBRenovaciones.SaveChanges();
                 llenar.llenarGridRenovaciones(Consultas.POLIZAS_RENOVADAS(Convert.ToInt32(Session["CodigoGestor"]),2, "",""), GridElRoble);
