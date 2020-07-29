5ad06e015eade9b70db4bbfb7d8ebe0d13111318
Jorge Laj <jorgelg132012@gmail.com>
2020-07-29 17:22:24 -0600

    agregando buscador inteligente

--- APP PRINCIPAL/Modulos/MdReclamos/autorizaciones.aspx.cs
+++ APP PRINCIPAL/Modulos/MdReclamos/autorizaciones.aspx.cs
@@ -22,21 +22,42 @@ public partial class Modulos_MdReclamos_autorizaciones : System.Web.UI.Page
 
     protected void btnBuscar_Click(object sender, EventArgs e)
     {
-        string consulta = "SELECT a.id as ID, " +
-            "r.poliza as Poliza,"+
-            "a.reportante as Reportante," +
-            "a.tipo_consulta as [Tipo Consulta]," +
-            "a.tipo_estado as [Estado], " +
-            "a.correo as Correo," +
-            "a.telefono as Telefono," +
-            "a.fecha_completa_commit as [Fecha]," +
-            "a.fecha_completa_cierre as [Fecha Cierre] " +
-            " FROM autorizaciones as a " +
-            "inner join reg_reclamos_medicos as r on a.id_reg_reclamos_medicos = r.id "+
-            "where (a.reportante like '%" + txtBuscar.Text + "%' or r.poliza like '%"+txtBuscar.Text+"%') and convert(date, a.fecha_commit,112) " +
-            "between '" + txtFechaInicio.Text + "' and '" + txtFechaFinal.Text + "'";
+        Cargar.llenarGrid(buscador(txtBuscar.Text), GridAutorizaciones);
+    }
 
-        Cargar.llenarGrid(consulta, GridAutorizaciones);
+    public string buscador(String dato)
+    {
+        string[] arreglo = dato.Split(" ".ToCharArray());
+        string sql = "";
+        if (arreglo.Length > 0)
+        {
+           sql = "SELECT a.id as ID, " +
+          "r.poliza as Poliza," +
+          "a.reportante as Reportante," +
+          "a.tipo_consulta as [Tipo Consulta]," +
+          "a.tipo_estado as [Estado], " +
+          "a.correo as Correo," +
+          "a.telefono as Telefono," +
+          "a.fecha_completa_commit as [Fecha]," +
+          "a.fecha_completa_cierre as [Fecha Cierre] " +
+          " FROM autorizaciones as a " +
+          "inner join reg_reclamos_medicos as r on a.id_reg_reclamos_medicos = r.id " +
+          "where a.reportante like '%" + arreglo[0] + "%' and convert(date, a.fecha_commit,112) " +
+          "between '" + txtFechaInicio.Text + "' and '" + txtFechaFinal.Text + "'";
+
+            if (arreglo.Length > 1)
+            {
+                for (int i = 1; i < arreglo.Length; i++)
+                {
+                    if (!String.IsNullOrEmpty(arreglo[i].Trim()))
+                    {
+                        sql += " and a.reportante like '%" + arreglo[i] + "%' ";
+                    }
+                }
+            }
+        }
+
+        return sql;
     }
 
     protected void GridAutorizaciones_SelectedIndexChanged(object sender, EventArgs e)
